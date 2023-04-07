48932ddbb71acfcaa46d4801e479695db40e5edd
DavidkasUnit <davidlapin99@gmail.com>
2023-04-07 13:15:01 +0300

    First Smarty commit

--- Home/Classes/Rendering/Rendering.php
+++ Home/Classes/Rendering/Rendering.php
@@ -3,13 +3,20 @@
 namespace Phpcourse\Myproject\Classes\Rendering;
 
 use Phpcourse\Myproject\Classes\Traits\DebugTrait;
+use Smarty;
 
 class Rendering{
     use DebugTrait;
     public function __construct(array $data){
         foreach ($data as $key => $value){
-            echo $key . ' = ' . $value . '<br>';
+            //echo $key . ' = ' . $value . '<br>';
             self::debugConsole($key . ' = ' . $value . '<br>');
         }
+        $smarty = new Smarty();
+        $smarty->setTemplateDir('templates/1');
+        $smarty->setConfigDir('templates/config');
+        $smarty->setCompileDir('templates/compile');
+        $smarty->setCacheDir('templates/cache');
+        $smarty->testInstall();
     }
 }
